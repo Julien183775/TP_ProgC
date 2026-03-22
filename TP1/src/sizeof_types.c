6facc0b73fcd65e1951e731ff6fac55aaf217637
Julien183775 <julien.valadier@2028.icam.fr>
2026-03-22 11:13:24 +0000

    Ajout de mes exercices

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    printf("Tailles des types de base (en octets) :\n");
+    printf("---------------------------------------\n");
+
+    // Types entiers
+    printf("char : %lu\n", sizeof(char));
+    printf("short : %lu\n", sizeof(short));
+    printf("int : %lu\n", sizeof(int));
+    printf("long int : %lu\n", sizeof(long int));
+    printf("long long int : %lu\n", sizeof(long long int));
+
+    // Types à virgule
+    printf("float : %lu\n", sizeof(float));
+    printf("double : %lu\n", sizeof(double));
+    printf("long double : %lu\n", sizeof(long double));
+
+    return 0;
+}
