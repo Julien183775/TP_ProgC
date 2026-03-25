55c7f50287b6a84ae407daef691086fcb2c53565
Julien183775 <julien.valadier@2028.icam.fr>
2026-03-25 17:41:40 +0000

    Ajout de mes exercices

---
+++ TP2/src/puissance.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;  // base
+    int b = 3;  // exposant
+    int resultat = 1;
+
+    // Calcul de a^b
+    for (int i = 0; i < b; i++) {
+        resultat = resultat * a;
+    }
+
+    // Affichage du résultat
+    printf("Le resultat de %d^%d est : %d\n", a, b, resultat);
+
+    return 0;
+}
