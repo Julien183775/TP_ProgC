877acaeff31c609bed3b895839b3292a94945a36
Julien183775 <julien.valadier@2028.icam.fr>
2026-03-29 18:05:03 +0000

    Ajout de mes exercices

--- TP5/src/client.c
+++ TP5/src/client.c
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: 2021 John Samuel
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
- *
  */
 
 #include <string.h>
@@ -18,15 +17,15 @@
 int etudiant_courant = 1;
 int somme_courante = 0;
 
+/* ================= SOCKET ================= */
+
 int envoie_operateur_numeros(int socketfd, char operateur, int num1, int num2)
 {
   char data[1024];
 
-  memset(data, 0, sizeof(data));
   sprintf(data, "calcule : %c %d %d", operateur, num1, num2);
 
-  int write_status = write(socketfd, data, strlen(data));
-  if (write_status < 0)
+  if (write(socketfd, data, strlen(data)) < 0)
   {
     perror("Erreur d'écriture");
     return -1;
@@ -41,89 +40,104 @@ int envoie_operateur_numeros_et_recois_resultat(int socketfd, char operateur, in
   int resultat = 0;
 
   if (envoie_operateur_numeros(socketfd, operateur, num1, num2) < 0)
-  {
     return -1;
-  }
 
   memset(data, 0, sizeof(data));
 
-  int read_status = read(socketfd, data, sizeof(data));
-  if (read_status < 0)
+  if (read(socketfd, data, sizeof(data)) < 0)
   {
     perror("Erreur de lecture");
     return -1;
   }
 
   if (sscanf(data, "calcule : %d", &resultat) == 1)
-  {
     return resultat;
-  }
 
   if (sscanf(data, "%d", &resultat) == 1)
-  {
     return resultat;
-  }
 
-  printf("Reponse invalide du serveur : %s\n", data);
+  printf("Reponse invalide: %s\n", data);
   return -1;
 }
 
+/* ================= FICHIERS ================= */
+
 int lire_note_fichier(const char *chemin)
 {
-  FILE *fichier = fopen(chemin, "r");
-  int note = -1;
+  FILE *f = fopen(chemin, "r");
+  int note;
 
-  if (fichier == NULL)
+  if (!f)
   {
-    perror("Erreur ouverture fichier");
+    perror("Erreur fichier");
     return -1;
   }
 
-  if (fscanf(fichier, "%d", &note) != 1)
-  {
-    printf("Erreur lecture du fichier: %s\n", chemin);
-    fclose(fichier);
-    return -1;
-  }
-
-  fclose(fichier);
+  fscanf(f, "%d", &note);
+  fclose(f);
   return note;
 }
 
-int lire_note_etudiant(int numero_etudiant, int numero_note)
+int lire_note_etudiant(int etu, int note)
 {
   char chemin[256];
-
-  sprintf(chemin, "../etudiant/%d/note%d.txt", numero_etudiant, numero_note);
+  sprintf(chemin, "../etudiant/%d/note%d.txt", etu, note);
   return lire_note_fichier(chemin);
 }
 
-void afficher_notes_etudiant_courant()
-{
-  int i;
-  int note;
+/* ================= AFFICHAGE ================= */
 
+void afficher_notes()
+{
   printf("Etudiant %d\n", etudiant_courant);
 
-  for (i = 1; i <= 5; i++)
+  for (int i = 1; i <= 5; i++)
   {
-    note = lire_note_etudiant(etudiant_courant, i);
-    if (note < 0)
-    {
-      printf("note%d = erreur\n", i);
-    }
-    else
-    {
-      printf("note%d = %d\n", i, note);
-    }
+    printf("note%d = %d\n", i, lire_note_etudiant(etudiant_courant, i));
   }
 
   printf("somme = %d\n", somme_courante);
 }
 
-int convertir_token_en_valeur(char *token, int *valeur)
+/* ================= CALCULS ================= */
+
+int somme_d_un_etudiant(int socketfd, int etu)
 {
-  int numero_note;
+  int n1 = lire_note_etudiant(etu, 1);
+  int n2 = lire_note_etudiant(etu, 2);
+  int n3 = lire_note_etudiant(etu, 3);
+  int n4 = lire_note_etudiant(etu, 4);
+  int n5 = lire_note_etudiant(etu, 5);
+
+  int s1 = envoie_operateur_numeros_et_recois_resultat(socketfd, '+', n1, n2);
+  int s2 = envoie_operateur_numeros_et_recois_resultat(socketfd, '+', n3, n4);
+  int s3 = envoie_operateur_numeros_et_recois_resultat(socketfd, '+', s1, s2);
+  int total = envoie_operateur_numeros_et_recois_resultat(socketfd, '+', s3, n5);
+
+  return total;
+}
+
+/* ⭐ MODIFICATION PRINCIPALE */
+int moyenne_des_5_etudiants(int socketfd)
+{
+  printf("Moyennes des 5 etudiants :\n");
+
+  for (int i = 1; i <= 5; i++)
+  {
+    int somme = somme_d_un_etudiant(socketfd, i);
+    int moyenne = envoie_operateur_numeros_et_recois_resultat(socketfd, '/', somme, 5);
+
+    printf("Moyenne etudiant %d = %d/20\n", i, moyenne);
+  }
+
+  return 0;
+}
+
+/* ================= TOKENS ================= */
+
+int convertir_token(char *token, int *valeur)
+{
+  int n;
 
   if (strcmp(token, "somme") == 0)
   {
@@ -131,162 +145,103 @@ int convertir_token_en_valeur(char *token, int *valeur)
     return 0;
   }
 
-  if (sscanf(token, "note%d", &numero_note) == 1)
+  if (sscanf(token, "note%d", &n) == 1)
   {
-    if (numero_note < 1 || numero_note > 5)
-    {
-      printf("Numero de note invalide: %s\n", token);
-      return -1;
-    }
-
-    *valeur = lire_note_etudiant(etudiant_courant, numero_note);
+    *valeur = lire_note_etudiant(etudiant_courant, n);
     return 0;
   }
 
   if (sscanf(token, "%d", valeur) == 1)
-  {
     return 0;
-  }
 
-  printf("Token invalide: %s\n", token);
   return -1;
 }
 
-int traite_commande_notes(int socketfd, char *message)
+int commande_notes(int socketfd, char *msg)
 {
-  char operateur;
-  char token1[100];
-  char token2[100];
-  int valeur1, valeur2;
-  int resultat;
+  char op, t1[50], t2[50], extra[50];
+  int v1, v2;
 
-  if (sscanf(message, " %c %99s %99s", &operateur, token1, token2) != 3)
-  {
+  if (sscanf(msg, " %c %s %s %s", &op, t1, t2, extra) != 3)
     return -1;
-  }
 
-  if (operateur != '+' && operateur != '-' && operateur != '*' && operateur != '/')
-  {
+  if (convertir_token(t1, &v1) < 0 || convertir_token(t2, &v2) < 0)
     return -1;
-  }
 
-  if (convertir_token_en_valeur(token1, &valeur1) < 0)
-  {
-    return -1;
-  }
+  int res = envoie_operateur_numeros_et_recois_resultat(socketfd, op, v1, v2);
 
-  if (convertir_token_en_valeur(token2, &valeur2) < 0)
-  {
-    return -1;
-  }
-
-  resultat = envoie_operateur_numeros_et_recois_resultat(socketfd, operateur, valeur1, valeur2);
-  if (resultat < 0)
-  {
-    return -1;
-  }
-
-  somme_courante = resultat;
-  printf("%d\n", somme_courante);
+  somme_courante = res;
+  printf("%d\n", res);
 
   return 0;
 }
 
+/* ================= MAIN LOOP ================= */
+
 int envoie_recois_message(int socketfd)
 {
+  char msg[1024];
   char data[1024];
-  char message[1024];
-  char operateur;
-  int num1, num2;
-  int nouvel_etudiant;
-
-  memset(data, 0, sizeof(data));
+  int etu;
 
   printf("> ");
+  fgets(msg, sizeof(msg), stdin);
+  msg[strcspn(msg, "\n")] = 0;
 
-  if (fgets(message, sizeof(message), stdin) == NULL)
+  /* choisir étudiant */
+  if (sscanf(msg, "etudiant %d", &etu) == 1)
   {
-    return -1;
-  }
-
-  message[strcspn(message, "\n")] = '\0';
-
-  if (sscanf(message, "etudiant %d", &nouvel_etudiant) == 1)
-  {
-    if (nouvel_etudiant < 1 || nouvel_etudiant > 5)
-    {
-      printf("Etudiant invalide\n");
-      return 0;
-    }
-
-    etudiant_courant = nouvel_etudiant;
+    etudiant_courant = etu;
     somme_courante = 0;
     printf("Etudiant %d selectionne\n", etudiant_courant);
     return 0;
   }
 
-  if (strcmp(message, "voir_notes") == 0)
+  /* voir notes */
+  if (strcmp(msg, "voir_notes") == 0)
   {
-    afficher_notes_etudiant_courant();
+    afficher_notes();
     return 0;
   }
 
-  if (strcmp(message, "reset") == 0)
+  /* reset */
+  if (strcmp(msg, "reset") == 0)
   {
     somme_courante = 0;
-    printf("Somme remise a 0\n");
     return 0;
   }
 
-  if (sscanf(message, "calcule : %c %d %d", &operateur, &num1, &num2) == 3)
+  /* ⭐ NOUVELLE COMMANDE */
+  if (strcmp(msg, "/ somme 5") == 0)
   {
-    int write_ok = envoie_operateur_numeros(socketfd, operateur, num1, num2);
-    if (write_ok < 0)
-    {
-      return -1;
-    }
+    return moyenne_des_5_etudiants(socketfd);
+  }
 
-    memset(data, 0, sizeof(data));
-
-    int read_status = read(socketfd, data, sizeof(data));
-    if (read_status < 0)
-    {
-      perror("Erreur de lecture");
-      return -1;
-    }
-
+  /* ancien format */
+  char op;
+  int a, b;
+  if (sscanf(msg, "calcule : %c %d %d", &op, &a, &b) == 3)
+  {
+    envoie_operateur_numeros(socketfd, op, a, b);
+    read(socketfd, data, sizeof(data));
     printf("%s\n", data);
     return 0;
   }
 
-  if (traite_commande_notes(socketfd, message) == 0)
-  {
+  /* commandes notes */
+  if (commande_notes(socketfd, msg) == 0)
     return 0;
-  }
 
-  strcpy(data, "message: ");
-  strcat(data, message);
-
-  int write_status = write(socketfd, data, strlen(data));
-  if (write_status < 0)
-  {
-    perror("Erreur d'écriture");
-    return -1;
-  }
-
-  memset(data, 0, sizeof(data));
-
-  int read_status = read(socketfd, data, sizeof(data));
-  if (read_status < 0)
-  {
-    perror("Erreur de lecture");
-    return -1;
-  }
-
+  /* message texte */
+  sprintf(data, "message: %s", msg);
+  write(socketfd, data, strlen(data));
+  read(socketfd, data, sizeof(data));
   printf("%s\n", data);
 
   return 0;
 }
+
+/* ================= MAIN ================= */
 
 int main()
 {
@@ -294,32 +249,20 @@ int main()
   struct sockaddr_in server_addr;
 
   socketfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (socketfd < 0)
-  {
-    perror("socket");
-    exit(EXIT_FAILURE);
-  }
 
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT);
   server_addr.sin_addr.s_addr = INADDR_ANY;
 
-  int connect_status = connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (connect_status < 0)
-  {
-    perror("connection serveur");
-    exit(EXIT_FAILURE);
-  }
+  connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
   printf("Client connecte au serveur.\n");
 
   while (1)
   {
     if (envoie_recois_message(socketfd) < 0)
-    {
       break;
-    }
   }
 
   close(socketfd);
